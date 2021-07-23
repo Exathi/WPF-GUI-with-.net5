c517b2116bab798438d087874d2c3e8a1174512b
Exathi <exathi.github@gmail.com>
2021-07-23 00:21:49 -0400

    Initial

---
+++ ActionHistory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace GUI_Playground
+{
+    /// <summary>
+    /// Object to log all button view actions
+    /// </summary>
+    class ActionHistory
+    {
+        public ActionHistory(DateTime dateTime, string description)
+        {
+            DateTime = dateTime;
+            Description = description;
+        }
+        public DateTime DateTime { get; set; }
+        public string Description { get; set; }
+    }
+}
